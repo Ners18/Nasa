68f09f936e3d265be50543b1c8ec03f279244307
Ners18 <nersan.yahi@gmail.com>
2024-09-16 16:54:52 +0300

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4 flex items-center justify-center">
-      <h1 className="text-xl">NASA App</h1>
+      <Link to="/Nasa" className="text-xl font-bold">
+        NASA App
+      </Link>
     </header>
   );
 };
